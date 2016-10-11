4e505d5cdd9d847c867653d92911829e3d6658cc
jgisson <ext-jerome.gisson@tessi.fr>
2016-10-11 09:29:10 +0200

    Init project by Spring boot Initializr

---
+++ src/main/java/jgisson/tests/springboot/AppWithJtaApplication.java
@@ -0,0 +1,12 @@
+package jgisson.tests.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWithJtaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWithJtaApplication.class, args);
+	}
+}
